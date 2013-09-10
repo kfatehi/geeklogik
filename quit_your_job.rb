63d5104fb30d28ed9addc548bf6ddd2a2387a8ce
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-09-10 14:53:03 -0700

    cleaner

--- quit_your_job.rb
+++ quit_your_job.rb
@@ -1,21 +1,29 @@
+
+
+
+
 # Page 68 - Should you quit your job?
 
-# Hours per day the song "Take This Job and Shove It" plays in your head
+# Hours per day the song 
+# "Take This Job and Shove It" plays in your head
 H = 1
 
 # Your age
 A = 24
 
-# How good is your current job? (1-10 with 10 being "ambassador to Italy")
+# How good is your current job? 
+# (1-10 with 10 being "ambassador to Italy")
 G = 7
 
 # How many kids do you have?
 K = 0
 
-# Your chances of quickly finding another job (1-10 with 10 being "already signed a new contract")
+# Your chances of quickly finding another job 
+# (1-10 with 10 being "already signed a new contract")
 P = 9
 
-# Your level of financial commitment (1-10 with 10 being "mortgaged into the next milennium")
+# Your level of financial commitment 
+# (1-10 with 10 being "mortgaged into the next milennium")
 F = 2
 
 # Monthly salary you could expect to recieve at a new job
